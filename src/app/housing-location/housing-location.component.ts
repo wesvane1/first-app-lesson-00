528e97a967b29f13ccf4f12e92b0a1159553f093
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-04-22 21:14:02 -0600

    initialized repo

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  // The "!" is there because the input is expecting a value to be passed, it allows it to be Non-Nullable.
+  @Input() housingLocation!: HousingLocation;
+}
